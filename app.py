67e60ff0dba2422204ab9233404b43c6605f5484
gilz <gilz@codevalue.net>
2019-06-26 05:49:19 +0300

    fix config source to crossInfra

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import configparser
 import json
 import os
 import sys
 import threading
+import time
+
+import requests
 from flask import Flask
 sys.path.append(os.path.abspath('../CrossInfra'))
 from RedisManager import connect
@@ -9,14 +13,17 @@ from BasicFunc import BasicFunc
 from Combination import Combination
 from ConfigManager import ConfigManager
 
+
 lst = []
 r = connect()
 app = Flask(__name__)
 listen = False
 redisCheckThread = None
+
 @app.route('/Selector/Start',methods=['POST'])
+@app.before_first_request
 def StartListen():
-    global redisCheckThread,listen,lst
+    global redisCheckThread, listen, lst
 
     comb = Combination()
     comb.InitCombinations()
@@ -28,6 +35,25 @@ def StartListen():
         return "Listening started"
     else:
         return "Already listening"
+
+def start_runner():
+    def start_loop():
+        not_started = True
+        while not_started:
+            try:
+                r = requests.get(configDef['url'])
+                if r.status_code == 200:
+                    print('Server started, quiting start_loop')
+                    not_started = False
+                print(r.status_code)
+            except:
+                print('Server not yet started')
+            time.sleep(2)
+
+    print('Started runner')
+    thread = threading.Thread(target=start_loop)
+    thread.start()
+
 
 @app.route('/Bulker/Stop/',methods=['POST'])
 def StopListen():
@@ -41,7 +67,7 @@ class RedisCheck(threading.Thread):
         threading.Thread.__init__(self)
     def run(self):
         while listen:
-            data = r.lpop(ConfigManager().GetVal('selector_subscribeOn'))
+            data = r.lpop(ConfigManager().GetVal('subscribeOn'))
             if data is not None:
                 s = data.decode("utf-8")
                 dataDic = json.loads(s)
@@ -54,11 +80,16 @@ class RedisCheck(threading.Thread):
                     "bars": barLst,
                     "templates": relevantTemplates
                 }
-                r.lpush(ConfigManager().GetVal('selector_publishOn'), json.dumps(barsTempRelated))
+                r.lpush(ConfigManager().GetVal('publishOn'), json.dumps(barsTempRelated))
 
 
 
 
 if __name__ == '__main__':
-    app.config['SERVER_NAME'] = ConfigManager().GetVal('selectorUrl')
+    global configDef
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    configDef = config['DEFAULT']
+    app.config['SERVER_NAME'] = configDef['url']
+    start_runner()
     app.run(debug=True)
